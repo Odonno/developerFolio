869abe6e4ee564bdf015f80efc956044fe0f01bb
Odonno <bottiau.david@laposte.net>
2019-12-31 15:57:49 +0100

    feat: update header section

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -15,7 +15,9 @@ export default function Greeting() {
             <SocialMedia />
             <div className="button-greeting-div">
               <Button text="Contact me" href="#contact" />
-              <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
+              {greeting.resumeLink &&
+                <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
+              }
             </div>
           </div>
         </div>
